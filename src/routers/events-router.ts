5bc532580a79e727c62c069310b8ac7885af6fac
gabrielsd <gabrielsprecherdutra@gmail.com>
2023-05-09 21:44:51 -0300

    test: implement basic tests

--- src/routers/events-router.ts
+++ src/routers/events-router.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import { getDefaultEvent } from '@/controllers';
+import { authenticateToken, validateBody } from '@/middlewares';
 
 const eventsRouter = Router();
 
-eventsRouter.get('/', getDefaultEvent);
+eventsRouter
+  .get('/', getDefaultEvent)
+  .get('/dates', authenticateToken, () => 0)
+  .get('/dates/:dateId', authenticateToken, () => 0)
+  .post('/subscribe/:eventId', authenticateToken, () => 0);
 
 export { eventsRouter };
--- tests/integration/event.test.ts
+++ tests/integration/event.test.ts
@@ -1,7 +1,8 @@
 import httpStatus from 'http-status';
 import supertest from 'supertest';
-import { createEvent } from '../factories';
-import { cleanDb } from '../helpers';
+import { TicketStatus } from '@prisma/client';
+import * as factory from '../factories';
+import { cleanDb, generateValidToken } from '../helpers';
 import app, { init } from '@/app';
 
 beforeAll(async () => {
@@ -19,7 +20,7 @@ describe('GET /event', () => {
   });
 
   it('should respond with status 200 and event data if there is an event', async () => {
-    const event = await createEvent();
+    const event = await factory.createEvent();
 
     const response = await server.get('/event');
 
@@ -32,5 +33,52 @@ describe('GET /event', () => {
       startsAt: event.startsAt.toISOString(),
       endsAt: event.endsAt.toISOString(),
     });
+  });
+});
+
+describe('GET /event/dates', () => {
+  it('should respond with status 200 and dates', async () => {
+    const event = await factory.createEvent();
+
+    const user = await factory.createUser();
+    const token = await generateValidToken(user);
+    const enrollment = await factory.createEnrollmentWithAddress(user);
+    const ticketType = await factory.createTicketTypeWithHotel();
+    const ticket = await factory.createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+    const payment = await factory.createPayment(ticket.id, ticketType.price);
+
+    const response = await server.get('/event/dates');
+
+    expect(response.status).toBe(httpStatus.OK);
+    expect(response.body).toEqual([
+      {
+        //to implement
+      },
+    ]);
+  });
+});
+
+describe('GET /event/dates/:dateId', () => {
+  it('should respond with status 200 and array of events for the date', async () => {
+    const event = await factory.createEvent();
+
+    const response = await server.get('/event/dates/{DATE_HERE}');
+
+    expect(response.status).toBe(httpStatus.OK);
+    expect(response.body).toEqual([
+      {
+        //to implement
+      },
+    ]);
+  });
+});
+
+describe('POST /event/subscribe/:eventId', () => {
+  it('should respond with status 200', async () => {
+    const event = await factory.createEvent();
+
+    const response = await server.post('/event/subscribe/${event.id}');
+
+    expect(response.status).toBe(httpStatus.OK);
   });
 });
